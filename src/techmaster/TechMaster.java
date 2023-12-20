972c11015549a8e22de6f0521eb3dc0b49a7b1b2
Flouver1z <S.Sm1rnof@yandex.ru>
2023-12-20 17:16:53 +0300

    init TechMaster

---
+++ src/techmaster/TechMaster.java
@@ -0,0 +1,16 @@
+package techmaster;
+
+/**
+ *
+ * @author SSm1r
+ */
+public class TechMaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
